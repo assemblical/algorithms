c4a20f65788f06d1d43ac4422d80c5dd9f355efc
Jerome Baensch <jerome.baensch@gmail.com>
2016-06-09 16:24:40 +0200

    insertion sort

---
+++ src/sorts/insertion_sort.hpp
@@ -0,0 +1,22 @@
+// insertion sort operating in O(n^2)
+#include <vector>
+
+template <typename container_t>
+inline void insertion_sort(container_t& container) noexcept {
+    const size_t containerSize = container.size();
+
+    if (containerSize < 2) {
+        return;
+    }
+
+    for (size_t i = 1; i < containerSize; ++i) {
+        auto key = container.at(i);
+
+        int j;
+        for (j = i - 1; j >= 0 && container.at(j) > key; --j) {
+            container.at(j + 1) = container.at(j);
+        }
+
+        container.at(j + 1) = key;
+    }
+}
