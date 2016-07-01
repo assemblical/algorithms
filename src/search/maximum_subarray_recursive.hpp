20f0d7d8051d164cc27e4729d0520a44ea43c621
Jerome Baensch <jerome.baensch@gmail.com>
2016-07-01 20:57:50 +0200

    subarray init

---
+++ src/search/maximum_subarray_recursive.hpp
@@ -0,0 +1,17 @@
+#ifndef ALGORITHMS_MAXIMUM_SUBARRAY_RECURSIVE_HPP
+#define ALGORITHMS_MAXIMUM_SUBARRAY_RECURSIVE_HPP
+
+namespace algorithm {
+
+    namespace detail {
+    
+        template <typename ContainerT>
+        inline void max_crossing_subarray(const ContainerT& container, int low, int mid, high) {
+            int left_sum = 0;
+        }
+        
+    } // detail
+    
+} // algorithm
+
+#endif //ALGORITHMS_MAXIMUM_SUBARRAY_RECURSIVE_HPP
